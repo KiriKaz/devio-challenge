1432072c2fdce106e96c946a0c5f0a25b2ad32c1
KiriKaz <igkepsi@gmail.com>
2022-05-22 18:50:13 -0300

    feat: add product routes

---
+++ controllers/products.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+const db = require('../db');
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  const data = db.getProducts();
+  return res.status(200).send(data);
+});
+
+router.get('/:productReference', (req, res) => {
+  const data = db.getDetailsAboutProduct(req.params.productReference);
+
+  if (data === -1) return res.status(500).json({ error: 'UNKNOWN_PRODUCT' });
+
+  return res.status(200).send(data);
+});
+
+module.exports = router;
--- routing.js
+++ routing.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const { Router } = require('express');
 
-const apiRouter = express.Router();
+const apiRouter = Router();
 const usersRouter = require('./controllers/users');
+const productsRouter = require('./controllers/products');
 
 apiRouter.get('/', (req, res) => {
   res.send('Hey hey, world!');
 });
 
 apiRouter.use('/users', usersRouter);
+apiRouter.use('/products', productsRouter);
 
 module.exports = apiRouter;

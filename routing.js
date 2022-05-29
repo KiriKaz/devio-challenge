3dd817346870e4666acc2d6356d762aa39f70e05
KiriKaz <igkepsi@gmail.com>
2022-05-28 22:37:03 -0300

    feat: add orders routing

--- routing.js
+++ routing.js
@@ -3,6 +3,7 @@ const { Router } = require('express');
 const apiRouter = Router();
 const usersRouter = require('./controllers/users');
 const productsRouter = require('./controllers/products');
+const ordersRouter = require('./controllers/orders');
 
 apiRouter.get('/', (req, res) => {
   res.send('Hey hey, world!');
@@ -10,5 +11,6 @@ apiRouter.get('/', (req, res) => {
 
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/products', productsRouter);
+apiRouter.use('/orders', ordersRouter);
 
 module.exports = apiRouter;

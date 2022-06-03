98139e069d16c5bcfa3ce93053f01abc810b1adb
KiriKaz <igkepsi@gmail.com>
2022-06-03 17:24:06 -0300

    chore: add dotenv.config() to globals

--- src/globals.ts
+++ src/globals.ts
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const PORT = process.env.PORT || 3301;
 export const SECRET = process.env.SECRET || 'I miss the rage';
 export const MONGODB_URI = process.env.MONGODB_URI || null;

049f3dbab066f9c97941f5731f20ea2cd5916332
KiriKaz <igkepsi@gmail.com>
2022-06-03 10:35:30 -0300

    refactor: remove unnecessary return value from products route

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -11,8 +11,6 @@ router.get('/', async (req, res) => {
 router.get('/:productReference', async (req, res) => {
   const data = await db.getDetailsAboutProduct(req.params.productReference);
 
-  if (data === false) return res.status(500).json({ error: 'UNKNOWN_PRODUCT' });
-
   return res.status(200).send(data);
 });
 

da6c5f2722a00076cb090284806fae4c19002a40
KiriKaz <igkepsi@gmail.com>
2022-06-03 17:24:28 -0300

    feat: finish implementing tests, for real

--- src/tests/stories.test.ts
+++ src/tests/stories.test.ts
@@ -1,8 +1,37 @@
 import { faker } from '@faker-js/faker';
+import mongoose from 'mongoose';
 import request from 'supertest';
 import db from '../db';
 import app from '../main';
-import { Order } from '../types';
+import { Cart, Order } from '../types';
+
+class sorryThisIsRequiredLol {
+  cart: Cart | null
+  order: Order | null
+
+  constructor() {
+    this.cart = null;
+    this.order = null;
+  }
+
+  async setCart(cart: Cart) {
+    this.cart = cart;
+  }
+
+  async getCart() {
+    return this.cart;
+  }
+
+  async setOrder(order: Order) {
+    this.order = order;
+  }
+
+  async getOrder() {
+    return this.order;
+  }
+}
+
+const globalVariables = new sorryThisIsRequiredLol();  // I couldn't find a better implementation in the short amount of time I had to optimize these tests.
 
 describe('Tests', () => {
   it('should return all products when querying /api/v1/products/', async () => {
@@ -28,48 +57,43 @@ describe('Tests', () => {
   
   describe('Order workflow', () => {
     const user = faker.fake('{{name.firstName}} {{name.lastName}}');
-    let cart;
-    let order: Order;
 
     describe('Client-facing', () => {
       it('should allow an unknown client to add a product to their cart', async () => {
-        console.log(user);
         const res = await request(app).post('/api/v1/orders/addProduct').send({
           name: user,
           product: "ESPFIL"
         });
-        expect(res.statusCode).toEqual(200);
-        cart = res.body.cart;
+        await expect(res.statusCode).toEqual(200);
+        await globalVariables.setCart(res.body.cart);
         
         const localCart = await db.getClientCurrentCart(user);
-        expect(localCart.products).toHaveLength(res.body.cart.products.length);
+        await expect(localCart.products).toHaveLength(res.body.cart.products.length);
       });
       
       it('should allow a known client to add a product to their cart', async () => {
-        console.log(user);
         const res = await request(app).post('/api/v1/orders/addProduct').send({
           name: user,
           product: "ESPTRA"
         });
-        expect(res.statusCode).toEqual(200);
-        cart = res.body.cart;
+        await expect(res.statusCode).toEqual(200);
+        await globalVariables.setCart(res.body.cart);
         
         const localCart = await db.getClientCurrentCart(user);
-        expect(localCart.products).toHaveLength(res.body.cart.products.length);
+        await expect(localCart.products).toHaveLength(res.body.cart.products.length);
       });
   
       it('should allow a known client to remove a product from their cart', async () => {
-        console.log(user);
         const res = await request(app).patch('/api/v1/orders/cart').send({
           op: 'removeproduct',
           clientRef: user,
           productRef: "ESPTRA"
         })
-        expect(res.statusCode).toEqual(200);
-        cart = res.body.cart;
+        await expect(res.statusCode).toEqual(200);
+        await globalVariables.setCart(res.body.cart);
   
         const localCart = await db.getClientCurrentCart(user);
-        expect(localCart.products).toHaveLength(res.body.cart.products.length);
+        await expect(localCart.products).toHaveLength(res.body.cart.products.length);
       });
 
       it('should allow a client to checkout', async () => {
@@ -79,62 +103,68 @@ describe('Tests', () => {
           observation: undefined,
         });
 
-        expect(res.status).toBe(201);
-        expect(res.body).toHaveProperty('order');
-        order = res.body.order;
-        // checkout(clientReference: string, paymentMethod: string, observation: string | undefined): Promise<Order> -> should allow checking out and create a new order and empty the cart
+        await expect(res.status).toBe(201);
+        await expect(res.body).toHaveProperty('order');
+        await globalVariables.setOrder(res.body.order);
       })
       
       it('should allow a client to see their cart', async () => {
         const res = await request(app).get(`/api/v1/orders/cart/${user}`);
 
-        expect(res.status).toBe(200);
-        expect(res.body).toHaveProperty('products');
-        expect(res.body.products).toHaveLength(0); // Post-checkout, it'll be empty.
+        await expect(res.status).toBe(200);
+        await expect(res.body).toHaveProperty('products');
+        await expect(res.body.products).toHaveLength(0); // Post-checkout, it'll be empty.
       })
-      
-      // getClientCurrentCart(clientReference: string): Promise<Cart> -> should return cart whether its empty or not
+    });
+    
+    describe('Kitchen-facing', () => {
+      it('should return details from a specific order when querying /api/v1/orders/:order', async () => {
+        const order = await globalVariables.getOrder();
+        const res = await request(app).get(`/api/v1/orders/${order!._id}`);
+        
+        await expect(res.status).toBe(200);
+        await expect(res.body).toHaveProperty("complete");
+      });
 
-      describe('Kitchen-facing', () => {
-        it('should return details from a specific order when querying /api/v1/orders/:order', async () => {
-          const res = await request(app).get(`/api/v1/orders/${order._id}`);
+      it('should return the order and mark it as complete when patching /api/v1/orders/:order with op \'complete\'', async () => {
+        const order = await globalVariables.getOrder();
+        const res = await request(app).patch(`/api/v1/orders/${order!._id}`).send({
+          op: "complete"
+        });
+        
+        await expect(res.status).toBe(200);
+        await expect(res.body).toHaveProperty('complete');
+        await expect(res.body.complete).toBeTruthy();
+      });
 
-          expect(res.status).toBe(200);
-          expect(res.body).toHaveProperty("complete");
+      it('should return the order and mark it as incomplete when patching /api/v1/orders/:order with op \'incomplete\'', async () => {
+        const order = await globalVariables.getOrder();
+        const res = await request(app).patch(`/api/v1/orders/${order!._id}`).send({
+          op: "incomplete"
         });
 
-        it('should return the order and mark it as complete when patching /api/v1/orders/:order with op \'complete\'', async () => {
-          const res = await request(app).patch(`/api/v1/orders/${order._id}`).send({
-            op: "complete"
-          });
+        await expect(res.status).toBe(200);
+        await expect(res.body).toHaveProperty('complete');
+        await expect(res.body.complete).toBeFalsy();
+      });
 
-          expect(res.status).toBe(200);
-          expect(res.body).toHaveProperty('complete');
-          expect(res.body.complete).toBeTruthy();
-        })
+      it('should return the order and mark edit the observation when patching /api/v1/orders/:order with op \'observation\'', async () => {
+        const order = await globalVariables.getOrder();
+        const res = await request(app).patch(`/api/v1/orders/${order!._id}`).send({
+          op: "observation",
+          observation: "No pickles."
+        });
 
-        it('should return the order and mark it as incomplete when patching /api/v1/orders/:order with op \'incomplete\'', async () => {
-          const res = await request(app).patch(`/api/v1/orders/${order._id}`).send({
-            op: "incomplete"
-          });
-
-          expect(res.status).toBe(200);
-          expect(res.body).toHaveProperty('complete');
-          expect(res.body.complete).toBeFalsy();
-        })
-
-        it('should return the order and mark edit the observation when patching /api/v1/orders/:order with op \'observation\'', async () => {
-          const res = await request(app).patch(`/api/v1/orders/${order._id}`).send({
-            op: "observation",
-            observation: "No pickles."
-          });
-
-          expect(res.status).toBe(200);
-          expect(res.body).toHaveProperty('observation');
-          expect(res.body.observation).toEqual("No pickles.");
-          // modifyOrderObservation(reference: string, observation: string | null): Promise<Order> -> should allow modification of incomplete order
-        })
+        await expect(res.status).toBe(200);
+        await expect(res.body).toHaveProperty('observation');
+        await expect(res.body.observation).toEqual("No pickles.");
+        // modifyOrderObservation(reference: string, observation: string | null): Promise<Order> -> should allow modification of incomplete order
       });
     });
   });
 })
+
+afterAll(async () => {
+  app.close();
+  await mongoose.disconnect();
+});
